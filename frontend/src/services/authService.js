2057b8a58cee6ebd0ef7224c1a40cdd9b6aa007a
6kEsio9 <konstantinevstatiev2005@gmail.com>
2023-05-28 13:04:06 +0300

    change exports

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 const url = 'http://localhost:3030/users';
 
-exports.login = async (data) => {
+export const login = async (data) => {
     const request = await fetch(`${url}/login`, {
         method: 'POST',
         headers: {
@@ -14,7 +14,7 @@ exports.login = async (data) => {
     return result;
 };
 
-exports.register = async (data) => {
+export const register = async (data) => {
     const request = await fetch(`${url}/register`, {
         method: 'POST',
         headers:{
@@ -28,13 +28,13 @@ exports.register = async (data) => {
     return result;
 };
 
-exports.getOne = async (userId) => {
+export const getOne = async (userId) => {
     const request = await fetch(`${url}/${userId}`);
     const result = await request.json();
     return result;
 };
 
-exports.addFriends = async (userId, friendId) => {
+export const addFriends = async (userId, friendId) => {
     const request = await fetch(`http://localhost:3030/friend/${userId}/${friendId}`);
     const result = await request.text();
     return result;
